3937759d4b0ed2918745f7b85e1dca0e4ef3be56
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-04-22 21:10:46 +0200

    First commit. Firestore dependencies implemented

---
+++ Schedulife/SchedulifeApp.swift
@@ -0,0 +1,31 @@
+//
+//  SchedulifeApp.swift
+//  Schedulife
+//
+//  Created by Oskar Lövstrand on 2024-04-22.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct SchedulifeApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
