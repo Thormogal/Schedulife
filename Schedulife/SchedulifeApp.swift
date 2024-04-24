c16dba3df5e1e8bcb34ff29b3cd714e01b83b414
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-04-24 21:01:49 +0200

    Added the first version of the HabitViewModel

--- Schedulife/SchedulifeApp.swift
+++ Schedulife/SchedulifeApp.swift
@@ -6,26 +6,24 @@
 //
 
 import SwiftUI
-import FirebaseCore
+import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseManager.shared.configure()
+        return true
+    }
 }
 
 @main
 struct SchedulifeApp: App {
-  // register app delegate for Firebase setup
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-  var body: some Scene {
-    WindowGroup {
-      NavigationView {
-        MainView()
-      }
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                MainView()
+            }
+        }
     }
-  }
 }
