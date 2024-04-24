8b747d0a9e162dabe3ed4be4b40bb0be6c26399e
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-04-24 21:14:52 +0200

    Added the first layout version of the app

--- Schedulife/Views/MainView.swift
+++ Schedulife/Views/MainView.swift
@@ -9,16 +9,70 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            OverviewView()
+                .tabItem {
+                    Label("Overview", systemImage: "house")
+                }
+
+            ScheduleView()
+                .tabItem {
+                    Label("Schedule", systemImage: "calendar")
+                }
+
+            StatsView()
+                .tabItem {
+                    Label("Stats", systemImage: "chart.bar")
+                }
         }
-        .padding()
     }
 }
 
-#Preview {
-    MainView()
+struct OverviewView: View {
+    @State private var showingAddHabit = false
+    
+    var body: some View {
+        NavigationView {
+            Text("Overview of Habits")
+                .navigationBarTitle("Habits", displayMode: .inline)
+                .navigationBarItems(trailing: addButton)
+        }
+    }
+    
+    var addButton: some View {
+        Button(action: {
+            showingAddHabit = true
+        }) {
+            Image(systemName: "plus")
+                .imageScale(.large)
+        }
+        .sheet(isPresented: $showingAddHabit) {
+            AddHabitView()
+        }
+    }
 }
+
+struct ScheduleView: View {
+    var body: some View {
+        Text("Schedule for Habits")
+    }
+}
+
+struct StatsView: View {
+    var body: some View {
+        Text("Statistics of Habits")
+    }
+}
+
+struct AddHabitView: View {
+    var body: some View {
+        Text("Add a new Habit")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
