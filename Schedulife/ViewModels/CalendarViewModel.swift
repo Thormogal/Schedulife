2f911829d621382827a099cb9c2499ad24a66714
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-05-10 14:29:56 +0200

    added a calendar to the app.

--- Schedulife/ViewModels/CalendarViewModel.swift
+++ Schedulife/ViewModels/CalendarViewModel.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+class CalendarViewModel: ObservableObject {
+    @Published var currentMonth: Date
+
+    init() {
+        currentMonth = Date()
+    }
+
+    func nextMonth() {
+        currentMonth = Calendar.current.date(byAdding: .month, value: 1, to: currentMonth)!
+    }
+
+    func previousMonth() {
+        currentMonth = Calendar.current.date(byAdding: .month, value: -1, to: currentMonth)!
+    }
+
+    func daysInMonth() -> [Date?] {
+        var days: [Date?] = []
+        let daysInMonth = Calendar.current.range(of: .day, in: .month, for: currentMonth)!
+        let firstDayOfMonth = startOfMonth()
+        let weekdayOfFirstDay = Calendar.current.component(.weekday, from: firstDayOfMonth)
+        
+        // First day is a monday
+        let weekdayOffset = (weekdayOfFirstDay + 5) % 7
+
+        // Add empty days before start of a new month
+        for _ in 0..<weekdayOffset {
+            days.append(nil)
+        }
+        
+        for day in daysInMonth {
+            if let date = Calendar.current.date(byAdding: .day, value: day - 1, to: firstDayOfMonth) {
+                days.append(date)
+            }
+        }
+        
+        return days
+    }
+
+    func startOfMonth() -> Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: currentMonth))!
+    }
+
+    func isToday(_ date: Date) -> Bool {
+        return Calendar.current.isDateInToday(date)
+    }
+}
+
+
